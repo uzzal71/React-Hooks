6e9ff4fc331b705fab5e7b1ebcffbc5861cf7114
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-12 15:50:11 +0600

    Building a fetch component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
-import { useTrees } from './';
+import { useFetch } from './useFetch';
 
-const App = () => {
-  const { trees } = useTrees();
+const App = ({ login }) => {
+
+  const { loading, data, error } = useFetch(`https://api.github.com/users/${login}`);
+
+  if (loading) return <h2>Loading...</h2>
+  if (error) return <pre>{error}</pre>
 
   return (
-    <div className="App">
-      <h1>Trees I've heard of</h1>
-      <ul>
-        {trees.map(tree => (
-          <li key={tree.id}>{tree.name}</li>
-        ))}
-      </ul>
+    <div>
+      <img src={data.avatar_url} alt={data.login} height="200"/>
+      <div>
+        {data.login && <h1>{data.login}</h1>}
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-import React, { createContext, useContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const TreesContext = createContext();
-
-export const useTrees = () => useContext(TreesContext);
-
-const trees = [
-  {id: 1, name: "Nasir Uddin"},
-  {id: 2, name: "Dip Saha"},
-  {id: 3, name: "Uzzal Roy"},
-  {id: 4, name: "Juwel Rana"},
-];
 
 ReactDOM.render(
-  <TreesContext.Provider value={{ trees }}>
-    <App />
-  </TreesContext.Provider>,
+    <App login="uzzal71"/>,
   document.getElementById('root')
 );
