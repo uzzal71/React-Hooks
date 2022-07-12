54b868f8093f922dd91b4c595aa023ebf284d2fb
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-12 13:56:52 +0600

    Creating controlled components with useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 const App = (props) => {
-  const sound = useRef();
-  const color = useRef();
+  const [sound, setSound] = useState();
+  const [color, setColor] = useState("#000000");
 
   const submit = (e) => {
     e.preventDefault();
-    const soundVal = sound.current.value;
-    const colorVal = color.current.value;
-    alert(`${soundVal} sounds like ${colorVal}`);
-    sound.current.value = "";
-    color.current.value = "";
+    alert(`${sound} sounds like ${color}`);
+
+    setSound("");
+    setColor("#000000");
   }
 
   return (
     <div className="App">
       <form onSubmit={submit}>
-        <input ref={sound} type="text" placeholder="sound..."/>
-        <input ref={color} type="color" />
+        <input value={sound} type="text" placeholder="sound..." onChange={(e) => setSound(e.target.value)}/>
+        <input value={color} type="color"  onChange={(e) => setColor(e.target.value)}/>
         <button>ADD</button>
       </form>
     </div>
