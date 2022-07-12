c3a205f942bed8c033b4ef422f0e4565735b6b3f
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-12 15:17:30 +0600

    Retrieving data with useContext

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { TreesContext } from './';
+import { useContext } from 'react';
 
 const App = () => {
+  const { trees } = useContext(TreesContext);
 
   return (
     <div className="App">
       <h1>Trees I've heard of</h1>
+      <ul>
+        {trees.map(tree => (
+          <li key={tree.id}>{tree.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
